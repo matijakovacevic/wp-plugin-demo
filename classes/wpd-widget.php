6165f881e289698d34e7eab6841f8a09cb8dd400
Matija Kovačević <mata987@gmail.com>
2015-02-26 23:21:40 +0100

    Add widget class skeleton

---
+++ classes/wpd-widget.php
@@ -0,0 +1,32 @@
+<?php
+
+class WPD_Widget extends WP_Widget {
+
+    /**
+     * Sets up the widgets name etc
+     */
+    public function __construct() {}
+
+    /**
+     * Outputs the content of the widget
+     *
+     * @param array $args
+     * @param array $instance
+     */
+    public function widget( $args, $instance ) {}
+
+    /**
+     * Outputs the options form on admin
+     *
+     * @param array $instance The widget options
+     */
+    public function form( $instance ) {}
+
+    /**
+     * Processing widget options on save
+     *
+     * @param array $new_instance The new options
+     * @param array $old_instance The previous options
+     */
+    public function update( $new_instance, $old_instance ) {}
+}
